b0c793bc9f1d322723aadd9dc453ab31662176ae
aikerim <aygerim.sauletkhan@gmail.com>
2017-10-04 04:18:19 -0700

    getElementByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,4 +7,21 @@
 var getElementsByClassName = function(className
 ) {
   // your code here
+  var root = document.body; 
+  var result = []; 
+
+  //traverse the tree 
+  function traverseTree(node){ 
+  	if(node.classList.contains(className)){ 
+  		result.push(node)
+  	}
+
+  	for(var i = 0; i < node.children.length; i++){
+  		traverseTree(node.children[i])
+  	}
+  }
+
+  traverseTree(root)
+  return result;
+
 };
